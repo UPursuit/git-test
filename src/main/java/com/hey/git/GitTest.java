de7a7dd2cf78e8cdd292eeb1b2c999335b090762
Hey <Hey@qq.com>
2022-08-31 15:19:42 +0800

    second commit

--- src/main/java/com/hey/git/GitTest.java
+++ src/main/java/com/hey/git/GitTest.java
@@ -6,5 +6,6 @@ package com.hey.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, git!");
+        System.out.println("hello, git2!");
     }
 }
