0a1a18584ae63eaec3473b1858626ba6eb4bcc0d
Hey <Hey@qq.com>
2022-08-31 15:20:52 +0800

    third commit

--- src/main/java/com/hey/git/GitTest.java
+++ src/main/java/com/hey/git/GitTest.java
@@ -7,5 +7,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello, git!");
         System.out.println("hello, git2!");
+        System.out.println("hello, git3!");
     }
 }
